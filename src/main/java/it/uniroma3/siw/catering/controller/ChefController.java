50d3dcd34ecb5464558201d912dd5839cc692c3a
Giordano Andreola <gio.andreola@stud.uniroma3.it>
2022-05-27 14:55:57 +0200

    commit

--- src/main/java/it/uniroma3/siw/catering/controller/ChefController.java
+++ src/main/java/it/uniroma3/siw/catering/controller/ChefController.java
@@ -12,60 +12,63 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class AdminController {
+public class ChefController {
     @Autowired
     private ChefService chefService;
 
     @Autowired
     private ChefValidator chefValidator;
 
-    @RequestMapping(value = "admin/chefs", method = RequestMethod.GET)
+    @RequestMapping(value = "admin/chef/list", method = RequestMethod.GET)
     public String getChefs(Model model) {
         List<Chef> chefs = chefService.findAll();
         model.addAttribute("chefs", chefs);
-        return "admin/chefs";
+        return "admin/chef/list";
     }
 
-    @RequestMapping(value = "admin/aggiungiChef", method = RequestMethod.GET)
+    @RequestMapping(value = "admin/chef/add", method = RequestMethod.GET)
     public String addChef (Model model) {
         model.addAttribute("chef", new Chef());
-        return "admin/chefForm";
+        return "admin/chef/form";
     }
 
-    @RequestMapping(value = { "/admin/aggiungiChef" }, method = RequestMethod.POST)
+    @RequestMapping(value = { "admin/chef/add" }, method = RequestMethod.POST)
     public String registerUser(@ModelAttribute("chef") Chef chef, BindingResult bindingResult, Model model) {
         this.chefValidator.validate(chef, bindingResult);
-
         if (!bindingResult.hasErrors()) {
             chefService.saveChef(chef);
             return "admin/home";
         }
-        return "admin/aggiungiChef";
+        return "admin/chef/form";
     }
 
-    @RequestMapping(value = "admin/chef/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "admin/chef/data/{id}", method = RequestMethod.GET)
     public String getChef(Model model, @PathVariable("id") Long id) {
         Chef chef = chefService.findById(id);
         model.addAttribute("chef", chef);
-        return "admin/chef";
+        return "admin/chef/data";
     }
 
-    @RequestMapping(value = "admin/deleteChef/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "admin/chef/delete/{id}", method = RequestMethod.GET)
     public String deleteChef(Model model, @PathVariable("id") Long id) {
         chefService.delete(id);
-        return "admin/chefs";
+        return getChefs(model);
     }
 
-    @RequestMapping(value = "admin/modifyChef/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "admin/chef/modify/{id}", method = RequestMethod.GET)
     public String modifyChef(@PathVariable("id") Long id, Model model) {
         Chef chef = chefService.findById(id);
         model.addAttribute("chef", chef);
-        return "/admin/modifyChef";
+        return "admin/chef/modify";
     }
 
-    @RequestMapping(value = "admin/confermaModifica", method = RequestMethod.POST)
-    public String confirmModify(@ModelAttribute("chef") Chef chef, Model model) {
-        chefService.saveChef(chef);
-        return getChefs(model);
+    @RequestMapping(value = "admin/chef/modify/confirm", method = RequestMethod.POST)
+    public String confirmModify(@ModelAttribute("chef") Chef chef, BindingResult bindingResult, Model model) {
+        this.chefValidator.validate(chef, bindingResult);
+        if (!bindingResult.hasErrors()) {
+            chefService.saveChef(chef);
+            return getChefs(model);
+        }
+        return "admin/chef/modify/"+chef.getId();
     }
 }
